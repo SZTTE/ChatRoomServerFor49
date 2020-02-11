4121881fbcdf9bf70ce5e8635c78501427d0aa9a
SZTTE <769084783@qq.com>
2020-02-12 00:32:22 +0800

    转发消息

---
+++ ChatRoomServer2/Connection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatRoomServer2
+{
+    public class Connection
+    {
+        private TcpClient client;
+        private delegate void bytesMethod(byte[] bs);
+        private static event bytesMethod messageComeEvent;//时间相当于一个委托收集器，可以一次运行所有的委托
+        NetworkStream networkStream;//每个实例的数据流
+        public Connection(System.Net.Sockets.TcpClient c)
+        {
+            client = c;
+        }
+        public void StartService()
+        {
+            Thread t = new Thread(ConnectionThread);
+            t.Start();
+            messageComeEvent += new bytesMethod(this.Send);//把这个实例的发送方法，交给事件统一管理
+        }
+        private void ConnectionThread()
+        {
+            Console.WriteLine("已经进入连接线程");
+            networkStream = client.GetStream();
+            while(true)//在这里保持收听，并且在取得新消息之后转发
+            {
+                try
+                {
+                    if (networkStream.CanRead)//当取得新信息，就广播出去
+                    {
+                        Byte[] data = new Byte[1000];
+                        int i = networkStream.Read(data, 0, data.Length);
+                        string rawString = System.Text.Encoding.UTF8.GetString(data, 0, i);
+                        Console.WriteLine("收取到新消息：");
+                        Console.WriteLine(rawString);
+                        if (!rawString.Contains('\x3'))
+                        {
+                            throw new System.IO.IOException();
+                        }
+
+                        messageComeEvent(data);
+                    }
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine("一个连接断开了");
+                    networkStream.Close();
+                    client.Dispose();
+                    return;
+                }
+            }
+        }
+        private void Send(byte[] bs)
+        {
+            if (client.Connected)
+            {
+                Console.WriteLine("转发了一次");
+                networkStream.Write(bs, 0, bs.Length);
+            }
+        }
+    }
+}
---
+++ ChatRoomServer2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatRoomServer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //初始化
+            TcpListener tcpListener = new TcpListener(IPAddress.Any,27105);
+            tcpListener.Start();
+            Console.WriteLine("服务建立完成，开始侦听");
+            while(true)
+            {
+                //侦听，接收后放进connection的新线程
+                TcpClient c = tcpListener.AcceptTcpClient();
+                Connection connection = new Connection(c);
+                connection.StartService();
+                Console.WriteLine("建立了新的连接");
+            }
+        }
+    }
+}
